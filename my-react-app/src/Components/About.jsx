a18fd4040a3daf6565349e15fa2f1176e732f7eb
ZyanyaMN <142449502+ZyanyaMN@users.noreply.github.com>
2023-10-04 14:40:49 -0600

    Add files via upload

---
+++ my-react-app/src/Components/About.jsx
@@ -0,0 +1,20 @@
+import './About.css';
+
+
+const About = () => {
+    return (
+        
+    <div class='about--container'>
+        <h1 class='name'>Nombre: Zyanya Martínez </h1>
+        <a class= 'mail' href = "mailto: zyanya_mn@hotmail.com">E-mail: zyanya_mn@hotmail.com</a>
+        <p class='info'>Sobre mi: Me gustan los animales, la naturaleza, <br></br>
+        la ciencia y tecnología, las matemáticas, la programación, <br></br>
+        el arte, la moda y el maquillaje.
+        </p>
+        <img  class='foto' src="../public/images/FotoZyan.jpg" alt="Foto Zyanya" />
+    </div>
+    )
+
+}
+
+export default About;
